60c980ab34e8f3b55c95eecd3b1e35f8418d932b
santiago_viale <santicbsn9@gmail.com>
2022-07-11 12:00:20 -0300

    desafio entregable

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 
-let datoObtenido= prompt("Cuantas veces quiere que lo felicitemos?")
-for (let i=1; i<=datoObtenido;i++){
-
-    alert("Te felicitamos" + i + "veces")
+// let datoObtenido= prompt("Cuantas veces quiere que lo felicitemos?")
+// for (let i=1; i<=datoObtenido;i++){
+//     alert("Te felicitamos " + i + " veces")
+// }
+function solicitarDatos(){
+    let nombreIngresado = prompt("Ingresar Nombre")
+    let apellidoIngresado = prompt("Ingresar Apellido")
+    let edadIngresada = prompt("Ingresar edad")
+    alert("Bienvenido a el/la señor/a " + nombreIngresado  + "" +  apellidoIngresado + ".Felicitaciones por sus " + edadIngresada + " años" )
 }
+solicitarDatos()
