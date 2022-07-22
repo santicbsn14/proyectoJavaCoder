9b746b4a9b5d75bb0c6dcd7eade10693cc0333e6
santiago_viale <santicbsn9@gmail.com>
2022-07-21 23:01:35 -0300

    entrega mala

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,7 @@
-
-// let datoObtenido= prompt("Cuantas veces quiere que lo felicitemos?")
-// for (let i=1; i<=datoObtenido;i++){
-//     alert("Te felicitamos " + i + " veces")
-// function solicitarDatos(){
-//     let nombreIngresado = prompt("Ingresar Nombre")
-//     let apellidoIngresado = prompt("Ingresar Apellido")
-//     let edadIngresada = prompt("Ingresar edad")
-//     alert("Bienvenido a el/la señor/a " + nombreIngresado  + "" +  apellidoIngresado + ".Felicitaciones por sus " + edadIngresada + " años" )
-// }
-// solicitarDatos()
-
-const cafes = [
-   {nombre: "Cafe Virginia molido" , precio: 1000} ,
-   {nombre: "Cafe Virginia en granos" , precio: 900} ,
-   {nombre: "Cafe Virginia torrado" , precio: 800} ,
-   {nombre: "Cafe Bonafide molido" , precio: 2000} ,
-   {nombre: "Cafe Bonafide  en granos" , precio: 1900} ,
-   {nombre: "Cafe Bonafide torrado" , precio: 1800} ,
-] 
-const cafeEconomico = cafes.filter ((el) => el.nombre.includes ("Virginia"))
-const cafeCalidad = cafes.filter ((el) => el.nombre.includes ("Bonafide") )
-console.log(cafeEconomico)
-console.log(cafeCalidad)
+let edad= prompt("Que edad tiene?")
+if(edad>=18){
+    alert("Bienvenido a nuestra cafeteria")
+}
+else{
+    alert("Usted es menor de edad, no deberia estar aqui")
+}
